4d6e0a2e251c1c7bb8067ed9478504a9331aaa14
Triple <sebastien.andrys@gmail.com>
2019-06-27 02:26:56 +0200

    users post commit 2h26

--- routes/users.js
+++ routes/users.js
@@ -44,23 +44,25 @@ router.post("/create-profile", (req, res) => {
     const userMail = req.body.email
     console.log('userMail', userData.email)
   
-    connexion.query(`SELECT email FROM users WHERE email = '${userMail}'`, (err, results) => {
+    connexion.query(`SELECT email FROM User WHERE email = '${userMail}'`, (err, results) => {
       if (err) {
   
         console.log(err);
         res.status(500).send("Erreur lors de la vérification de l'email");
       } else if (results.length > 0) {
+        console.log("froggy is so high")
         res.status(409, 'L\'email existe déja dans la base de donnée')
       } 
       else {
         console.log(results)
-        connexion.query('INSERT INTO users SET ?', [userData], (err, results) => {
+        connexion.query('INSERT INTO User SET ?', [userData], (err, results) => {
           if (err) {
             console.log(err);
+            console.log(userData)
             res.status(500).send("Erreur lors de la creation de l'utilisateur");
           }
           else {
-            res.status(200, 'Utilisateur ajouté a la base de donnée')
+            res.status(200, 'Utilisateur ajouté à la base de donnée')
           } 
         });
       } 
@@ -76,13 +78,13 @@ router.post("/login", (req, res) => {
     const userEmail = req.body.email 
     const userPw = req.body.password
   
-    connexion.query(`SELECT email FROM users WHERE EXISTS (SELECT email FROM users WHERE email = '${userEmail}'`, (err, results) => {
+    connexion.query(`SELECT email FROM User WHERE EXISTS (SELECT email FROM users WHERE email = '${userEmail}'`, (err, results) => {
       
       if (err) {
         console.error(err)
         res.status(401).send("Vous n'avez pas de compte")
       } else {
-        connexion.query(`SELECT password FROM users WHERE email = '${userEmail}' AND password = '${userPw}'`, (err, results) => {
+        connexion.query(`SELECT password FROM User WHERE email = '${userEmail}' AND password = '${userPw}'`, (err, results) => {
           if(err) {
             console.error(err)
             res.status(401).send("Mauvais mot de passe")
